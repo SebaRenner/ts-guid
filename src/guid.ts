10bc20380902c33fca662a7647b712a053ceeba5
SebaRenner <sebastian.renner@gmx.ch>
2024-05-19 16:01:32 +0200

    added static empty property

--- src/guid.ts
+++ src/guid.ts
@@ -7,7 +7,15 @@ export class Guid {
         this._value = uuid;
     }
 
+    public get value(): UUID {
+        return this._value;
+    }
+
     public static newGuid(): Guid {
         return new Guid(randomUUID()); 
+    }
+
+    public static get empty(): Guid {
+        return new Guid('00000000-0000-0000-0000-000000000000')
     }
 }
--- tests/guid.test.ts
+++ tests/guid.test.ts
@@ -12,13 +12,26 @@ describe('Guid', () => {
 
     it('should create a new guid instance with a unique identifier', () => {
         // arrange
-        const mockUUID = "123e4567-e89b-12d3-a456-426614174000";
-        (randomUUID as jest.MockedFunction<typeof randomUUID>).mockReturnValue(mockUUID);
+        const guidValue = '2e405af9-a1e8-4f09-8122-93609a9a589e';
+        (randomUUID as jest.Mock).mockReturnValue(guidValue);
 
         // act
         const guid = Guid.newGuid();
 
         // assert
         expect(guid).toBeInstanceOf(Guid);
+        expect(guid.value).toBe(guidValue)
+    });
+
+    it('should create a new guid instance with an empty guid', () => {
+        // arrange
+        const emptyGuid = '00000000-0000-0000-0000-000000000000';
+
+        // act
+        const guid = Guid.empty;
+
+        // assert
+        expect(guid).toBeInstanceOf(Guid);
+        expect(guid.value).toBe(emptyGuid);
     });
 });
